1df22657317b736deca7ce2bbb7e404177a3b040
Steven Dean <sdlanglois1@gmail.com>
2020-05-23 20:50:58 -0400

    everything good. synthax updates.

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,21 @@ USE employeeDB;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name VARCHAR(30), --to hold department name
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  title VARCHAR(30), --to hold role title
-  salary DECIMAL, --to hold role salary
-  department_id INT --to hold reference to department role belongs to
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT 
 );
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30), --to hold employee first name
-  last_name VARCHAR(30), --to hold employee last name
-  role_id INT, --to hold reference to role employee has
-  manager_id INT --to hold reference to another employee that manages the current employee. This field may be null if the employee has no manager
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
 );
 
